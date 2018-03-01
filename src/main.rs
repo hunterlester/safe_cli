303ffd95515075927d7b52f6980d8f3ef13a8b16
hunterlester <guilfordhunter@gmail.com>
2018-03-01 11:25:37 -0800

    chore/safe: initial safe interface impl

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,42 @@
 extern crate clap;
-use clap::{Arg, App};
+extern crate safe_authenticator;
+extern crate zxcvbn;
+
+use clap::{ Arg, App };
+use safe_authenticator::{ Authenticator };
+use zxcvbn::zxcvbn;
+use std::io;
+
+fn connected(auth: Authenticator) -> () {
+  println!("Logged in to SAFE network.")
+}
+
+fn validate_cred(cred: String) -> String {
+    println!("Please choose a {}:", &cred);
+    let mut secret = String::new();
+    io::stdin().read_line(&mut secret).expect("Please enter valid string");;
+    let secret_strength = zxcvbn(&mut secret, &[]).unwrap();
+    println!("\nInteresting information about your {}:\n
+      Estimated number of guesses needed to crack: {:?}\n
+      Estimated number of seconds needed with fastest cracking method: {:?}\n
+      In short, it would take with quickest cracking method: {:?}\n
+      On a scale of 0-4, your score is: {:?}\n", &cred, &secret_strength.guesses, &secret_strength.crack_times_seconds.offline_fast_hashing_1e10_per_second, &secret_strength.crack_times_display.offline_fast_hashing_1e10_per_second, &secret_strength.score);
+
+    if secret_strength.score <= 2 {
+      println!("\nYour {} is not strong enough.\n Here is how to make it stronger: \n{:?}", &cred, &secret_strength.feedback.unwrap());
+      validate_cred(cred)
+    } else {
+      println!("Please type in your {} again to confirm:", &cred);
+      let mut secret_compare = String::new();
+      io::stdin().read_line(&mut secret_compare).expect("Please enter valid string");
+      if &mut secret == &mut secret_compare {
+        secret
+      } else {
+        println!("{} does not match. Starting over...", &cred);
+        validate_cred(cred)
+      }
+    }
+}
 
 fn main() {
     let matches = App::new("safecli")
@@ -18,24 +55,33 @@ fn main() {
                                .short("c")
                                .long("create_acc")
                                .takes_value(true)
-                               .multiple(true)
-                               .number_of_values(3)
-                               .value_names(&["username", "password", "invitation"])
+                               .number_of_values(1)
+                               .value_names(&["invitation"])
                                .help("Create new account with SAFE network with name, password, and invitation key"))
                           .get_matches();
 
     match matches.values_of("login") {
       Some(value) => {
         let args: Vec<&str> = value.collect();
-        println!("{:?}, {:?}", &args[0], &args[1]);
+        match Authenticator::login(args[0], args[1], || println!("Disconnected from network")) {
+          Ok(auth) =>  connected(auth),
+          Err(auth_error) => println!("Login failed: {:?}", &auth_error),
+        }
       },
       None => (),
     }
 
-    match matches.values_of("create_acc") {
-      Some(value) => {
-        let args: Vec<&str> = value.collect();
-        println!("{:?}, {:?}, {:?}", &args[0], &args[1], &args[2]);
+    match matches.value_of("create_acc") {
+      Some(invite) => {
+          println!("invite code: {:?}", &invite);
+          let secret = validate_cred(String::from("secret"));
+          println!("\u{2705}Valid secret");
+          let password = validate_cred(String::from("password"));
+          println!("\u{2705}Valid password");
+          match Authenticator::create_acc(secret, password, String::from(invite), || println!("Disconnected from network")) {
+            Ok(auth) =>  connected(auth),
+            Err(auth_error) => println!("Failed to create account: {:?}", &auth_error),
+          }
       },
       None => (),
     }
