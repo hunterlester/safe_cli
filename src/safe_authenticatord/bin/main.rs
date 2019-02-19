a4e3d7afd5d7d4b333bab3a95834fe1420988942
hunterlester <guilfordhunter@gmail.com>
2019-02-19 00:19:52 -0800

    feat/safe_authenticator: removes commented out safe_authenticator components, breaks compilation

--- src/safe_authenticatord/bin/main.rs
+++ src/safe_authenticatord/bin/main.rs
@@ -1,65 +1,58 @@
 extern crate actix_web;
 extern crate console;
-// extern crate safe_authenticator;
+extern crate safe_authenticator;
 
 use actix_web::{http::Method, server, App, HttpRequest, Path};
 use console::style;
-// use safe_authenticator::{AuthError, Authenticator};
+use safe_authenticator::{AuthError, Authenticator};
 use std::sync::{Arc, Mutex};
 
 fn create_acc(info: Path<(String, String, String)>, req: HttpRequest) -> &'static str {
-    println!("req object: {:?}", &req);
-    // let mut handle = *(req.state().handle.lock().unwrap());
-    // match Authenticator::create_acc(info.0, info.1, info.2, || {
-    //     println!("{}", style("Disconnected from network").red().bold())
-    // }) {
-    //     Ok(auth) => {
-    //         handle = Some(Ok(auth));
-    //         "Account created and logged in to SAFE network."
-    //     }
-    //     Err(auth_error) => {
-    //         handle = Some(Err(auth_error));
-    //         format!("Failed to create account: {}", &auth_error).as_str()
-    //     }
-    // }
+    let mut handle = *(req.state().handle.lock().unwrap());
+    match Authenticator::create_acc(info.0, info.1, info.2, || {
+        println!("{}", style("Disconnected from network").red().bold())
+    }) {
+        Ok(auth) => {
+            handle = Some(Ok(auth));
             "Account created and logged in to SAFE network."
+        }
+        Err(auth_error) => {
+            handle = Some(Err(auth_error));
+            format!("Failed to create account: {}", &auth_error).as_str()
+        }
+    }
 }
 
 fn login(info: Path<(String, String)>, req: HttpRequest) -> &'static str {
-    println!("Login info: {:?}", info);
-    println!("req object: {:?}", &req);
-    // let mut handle = *(req.state().handle.lock().unwrap());
-    // match Authenticator::login(info.0, info.1, || {
-    //     println!("{}", style("Disconnected from network").red().bold())
-    // }) {
-    //     Ok(auth) => {
-    //         handle = Some(Ok(auth));
-    //         "Logged in to SAFE network."
-    //     }
-    //     Err(auth_error) => {
-    //         handle = Some(Err(auth_error));
-    //         format!("Login failed: {}", &auth_error).as_str()
-    //     }
-    // }
+    let mut handle = *(req.state().handle.lock().unwrap());
+    match Authenticator::login(info.0, info.1, || {
+        println!("{}", style("Disconnected from network").red().bold())
+    }) {
+        Ok(auth) => {
+            handle = Some(Ok(auth));
             "Logged in to SAFE network."
+        }
+        Err(auth_error) => {
+            handle = Some(Err(auth_error));
+            format!("Login failed: {}", &auth_error).as_str()
+        }
+    }
 }
 
 fn index(req: HttpRequest) -> &'static str {
-    println!("req object: {:?}", &req);
     "Hello, world!"
 }
 
-// struct Authenticator {
-//     handle: Arc<Mutex<Option<Result<Authenticator, AuthError>>>>
-// }
+struct Authenticator {
+    handle: Arc<Mutex<Option<Result<Authenticator, AuthError>>>>
+}
 
 fn main() {
-    // let handle: Arc<Mutex<Option<Result<Authenticator, AuthError>>>> =
-        // Arc::new(Mutex::new(None));
+    let handle: Arc<Mutex<Option<Result<Authenticator, AuthError>>>> =
+        Arc::new(Mutex::new(None));
 
     server::new(
-        // || App::with_state(Authenticator{handle: handle.clone()})
-        || App::new() 
+        || App::with_state(Authenticator{handle: handle.clone()})
            .route("/", Method::GET, index)
            .route("/login/{locator}/{password}", Method::POST, login)
            .route("/create_acc/{locator}/{password}/{invite}", Method::POST, create_acc)
